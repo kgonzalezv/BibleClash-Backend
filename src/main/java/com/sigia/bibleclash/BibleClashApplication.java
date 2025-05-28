d6763057dade5c465a0fe0c028a70055952babff
kgonzalez <katiuska012011@gmail.com>
2025-05-28 14:44:56 -0400

    first commit

---
+++ src/main/java/com/sigia/bibleclash/BibleClashApplication.java
@@ -0,0 +1,13 @@
+package com.sigia.bibleclash;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BibleClashApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BibleClashApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sigia/bibleclash/BibleClashApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sigia.bibleclash;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BibleClashApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
